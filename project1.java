bc841692d290b45b8fd2a4ae1329373cda7a6c33
root <root@ip-172-31-31-2.us-east-2.compute.internal>
2022-01-12 01:20:01 +0000

    First Commit

---
+++ project1.java
@@ -0,0 +1,10 @@
+
+
+ class project1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World"); 
+    System.out.println("In order to run java programs on Ubuntu you need to download java and javac - the compilier"); 
+    System.out.println("nano project_name, javac project_name, java project_name");
+    System.out.println("The above command is how the process should look");
+  }
+}
